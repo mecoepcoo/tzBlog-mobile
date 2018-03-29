d74145923911578177daa6d8f7de4d8442b2f4d2
mecoepcoo <340349237@qq.com>
2018-03-30 00:18:21 +0800

    1803300018

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
    <my-header></my-header>
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
     <router-view/>
   </div>
 </template>
@@ -16,13 +16,13 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #666;
+  font-size: .16rem;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,49 @@
 <template>
-  <div id="my-header">header</div>
+  <div id="my-header">
+    <div class="header-avatar"></div>
+    <div class="title">~ 天真呀 ~</div>
+    <ul class="nav">
+      <li>&nbsp;首页&nbsp;</li>
+      <li>&nbsp;归档&nbsp;</li>
+      <li>&nbsp;关于&nbsp;</li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
 
 }
 </script>
+<style lang="less" scoped>
+@import "../assets/init.less";
+@fs: 16px;
+#my-header {
+  height: 2rem;
+  padding: .4rem 0;
+  background-color: #f1f1f1;
+  .header-avatar {
+    margin: 0 auto;
+    width: .92rem;
+    height: .92rem;
+    background-image: url("../assets/avatar.jpg");
+    background-size: cover;
+    background-position: center;
+    border: 1px solid #eee;
+    .border-radius(1rem);
+  }
+  .title {
+    margin: .2rem 0;
+    font-size: @fs - 2;
+  }
+  .nav {
+    padding: .1rem 0;
+    font-size: @fs - 2;
+    li {
+      display: inline;
+      margin: .12rem;
+      color: #888;
+      border-bottom: 1px solid;
+    }
+  }
+}
+</style>
