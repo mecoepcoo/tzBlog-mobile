49dd5d03392711070fedf01d84adea215bfa45e9
tianzhen <340349237@qq.com>
2018-11-20 18:28:16 +0800

    修改标题

--- src/router/index.js
+++ src/router/index.js
@@ -6,22 +6,39 @@ import About from '@/components/About'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
+      meta: {
+        title: '首页-天真博客'
+      },
       component: Home
     },
     {
       path: '/archives',
       name: 'Archives',
+      meta: {
+        title: '归档-天真博客'
+      },
       component: Archives
     },
     {
       path: '/about',
       name: 'About',
+      meta: {
+        title: '关于-天真博客'
+      },
       component: About
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // Change doc title
+  document.title = to.meta.title || '天真博客'
+  next()
+})
+
+export default router
